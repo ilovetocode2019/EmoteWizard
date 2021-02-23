ee17fa5a1895468d22222e7c7ee556464f63494e
ilovetocode2019 <54194229+ilovetocode2019@users.noreply.github.com>
2021-02-22 17:19:02 -0800

    Better admin cog

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,77 +1,131 @@
 import discord
-from discord.ext import commands, menus, tasks
+from discord.ext import commands, tasks
 
+import asyncio
 import traceback
+import psutil
+import humanize
+import importlib
 import re
 import os
-import asyncio
+import sys
 import subprocess
 import time
 import traceback
 import io
 import pkg_resources
-from jishaku.codeblocks import codeblock_converter
+from jishaku import codeblocks, paginators, shell
 
 from .utils import formats, menus
 
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.update_loop.start()
+        self.hidden = True
 
-    async def cog_check(self, ctx):
-        return await commands.is_owner().predicate(ctx)
+        self.outdated_packages = []
+        self.update_packages_loop.start()
 
     def cog_unload(self):
-        self.update_loop.cancel()
+        self.update_packages_loop.cancel()
+
+    async def cog_check(self, ctx):
+        return await self.bot.is_owner(ctx.author)
 
     @commands.command(name="reload", description="Reload an extension")
-    @commands.is_owner()
     async def reload(self, ctx, extension):
         try:
             self.bot.reload_extension(extension)
-            await ctx.send(f"**:repeat: Reloaded** `{extension}`")
-        except Exception as e:
-            full = "".join(traceback.format_exception(type(e), e, e.__traceback__, 1))
-            await ctx.send(f"**:warning: Extension `{extension}` not reloaded.**\n```py\n{full}```")
+            await ctx.send(f":repeat: Reloaded `{extension}`")
+        except commands.ExtensionError as exc:
+            full = "".join(traceback.format_exception(type(exc), exc, exc.__traceback__, 1))
+            await ctx.send(f":warning: Couldn't reload `{extension}`\n```py\n{full}```")
+
+    @commands.command(name="load", description="Load an extension")
+    async def load(self, ctx, extension):
+        try:
+            self.bot.load_extension(extension)
+            await ctx.send(f":inbox_tray: Loaded `{extension}`")
+        except commands.ExtensionError as exc:
+            full = "".join(traceback.format_exception(type(exc), exc, exc.__traceback__, 1))
+            await ctx.send(f":warning: Couldn't load `{extension}`\n```py\n{full}```")
+
+    @commands.command(name="unload", description="Unload an extension")
+    async def unload(self, ctx, extension):
+        try:
+            self.bot.unload_extension(extension)
+            await ctx.send(f":out_tray: Unloaded `{extension}`")
+        except commands.ExtensionError as exc:
+            full = "".join(traceback.format_exception(type(exc), exc, exc.__traceback__, 1))
+            await ctx.send(f":warning: Couldn't unload `{extension}`\n```py\n{full}```")
 
     @commands.command(name="update", description="Update the bot")
     async def update(self, ctx):
-        await ctx.trigger_typing()
+        async with ctx.typing():
+            # Run git pull to update bot
+            process = await asyncio.create_subprocess_shell("git pull", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = await process.communicate()
+            text = stdout.decode()
 
-        regex = re.compile(r"\s*(?P<filename>.+?)\s*\|\s*[0-9]+\s*[+-]+")
+            # Find modules that need reloading
+            modules = []
+            regex = re.compile(r"\s*(?P<filename>.+?)\s*\|\s*[0-9]+\s*[+-]+")
+            files = regex.findall(text)
 
-        process = await asyncio.create_subprocess_shell("git pull", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = await process.communicate()
-        text = stdout.decode()
+            for file in files:
+                root, module = os.path.splitext(file)
+                if module != ".py":
+                    continue
+                if root.startswith("cogs/") and root.count("/") == 1:
+                    modules.append((root.count("/")-1, root.replace("/", ".")))
 
-        files = regex.findall(text)
-        cogs = []
-        for file in files:
-            root, ext = os.path.splitext(file)
-            if root.startswith("cogs/") and root.count("/") == 1 and ext == ".py":
-                cogs.append(root.replace("/", "."))
+            modules.sort(reverse=True)
 
-        if not cogs:
-            return await ctx.send("No cogs to update")
+        if not modules:
+            return await ctx.send("Nothing to update")
 
-        cogs_text = "\n".join(cogs)
-        result = await menus.Confirm(f"Are you sure you want to update the following cogs:\n{cogs_text}").prompt(ctx)
+        joined = "\n".join([module for is_module, module in modules])
+        result = await menus.Confirm(f"Are you sure you want to update the followings modules?\n{joined}").prompt(ctx)
         if not result:
-            return await ctx.send(":x: Aborting")
+            return await ctx.send("Aborting")
 
-        text = ""
-        for cog in cogs:
-            try:
-                self.bot.reload_extension(cog)
-                text += f"\n:white_check_mark: {cog}"
-            except:
-                text += f"\n:x: {cog}"
+        # Reload all the modules
+        results = []
+        for is_module, module in modules:
+            if is_module:
+                try:
+                    lib = sys.modules[module]
+                    try:
+                        importlib.reload(lib)
+                        results.append((True, module))
+                    except:
+                        results.append((False, module))
+                except KeyError:
+                    results.append((None, module))
 
-        await ctx.send(text)
+            else:
+                try:
+                    try:
+                        self.bot.reload_extension(module)
+                    except commands.ExtensionNotLoaded:
+                        self.bot.load_extension(module)
+                    results.append((True, module))
+                except:
+                    results.append((False, module))
+
+        message = ""
+        for status, module in results:
+            if status == True:
+                message += f"\n:white_check_mark: {module}"
+            elif status == False:
+                message += f"\n:x: {module}"
+            else:
+                message += f"\n:zzz: {module}"
+
+        await ctx.send(message)
 
     @commands.command(name="sql", description="Run some sql")
-    async def sql(self, ctx, *, code: codeblock_converter):
+    async def sql(self, ctx, *, code: codeblocks.codeblock_converter):
         _, query = code
 
         execute = query.count(";") > 1
@@ -108,19 +162,31 @@ class Admin(commands.Cog):
         except discord.HTTPException:
             await ctx.send(file=discord.File(io.BytesIO(str(results).encode("utf-8")), filename="result.txt"))
 
+    @commands.command(name="process", description="View system stats", aliases=["system", "health"])
+    async def process(self, ctx):
+        em = discord.Embed(title="Process", color=0x96c8da)
+        em.add_field(name="CPU", value=f"{psutil.cpu_percent()}% used with {formats.plural(psutil.cpu_count()):CPU}")
+
+        mem = psutil.virtual_memory()
+        em.add_field(name="Memory", value=f"{humanize.naturalsize(mem.used)}/{humanize.naturalsize(mem.total)} ({mem.percent}% used)")
+
+        disk = psutil.disk_usage("/")
+        em.add_field(name="Disk", value=f"{humanize.naturalsize(disk.used)}/{humanize.naturalsize(disk.total)} ({disk.percent}% used)")
+
+        await ctx.send(embed=em)
+
     @commands.command(name="logout", description="Logout the bot")
     @commands.is_owner()
     async def logout(self, ctx):
         await ctx.send(":wave: Logging out")
         await self.bot.logout()
 
-    @tasks.loop(hours=10)
-    async def update_loop(self):
+    async def get_outdated_packages(self, wait=None):
         installed = [
             "jishaku",
             "asyncpg",
             "humanize",
-            "Pillow"
+            "Pillow",
         ]
 
         outdated = []
@@ -134,20 +200,31 @@ class Admin(commands.Cog):
                 if current_version != pypi_version:
                     outdated.append((package, current_version, pypi_version))
             except Exception as exc:
-                traceback.print_exception(type(exc), exc, exc.__traceback__)
+                traceback.print_exception(type(exc), exc, exc.__traceback__,file=sys.stderr)
 
-            await asyncio.sleep(5)
+            if wait:
+                await asyncio.sleep(wait)
+
+        return outdated
+
+    @tasks.loop(hours=10)
+    async def update_packages_loop(self):
+        """Checks for outdated packages every 10 hours."""
+
+        outdated = await self.get_outdated_packages(wait=5)
+        self.outdated_packages = outdated
 
         if outdated:
             joined = " ".join([package[0] for package in outdated])
-            em = discord.Embed(title="Outdated Packages", description=f"Update with `jsk sh venv/bin/pip install -U {joined}`\n", color=discord.Color.blurple())
+            em = discord.Embed(title="Outdated Packages", description=f"Update with `jsk sh {sys.executable} -m pip install -U {joined}`\n", color=0x96c8da)
+
             for package in outdated:
                 em.description += f"\n{package[0]} (Current: {package[1]} | Latest: {package[2]})"
 
             await self.bot.console.send(embed=em)
 
-    @update_loop.before_loop
-    async def before_update_loop(self):
+    @update_packages_loop.before_loop
+    async def before_update_packages_loop(self):
         await self.bot.wait_until_ready()
 
 def setup(bot):
